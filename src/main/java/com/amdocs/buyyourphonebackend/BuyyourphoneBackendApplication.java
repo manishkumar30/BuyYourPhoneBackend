cec939852af2d6676b6517bf115ce34c3af2af00
Manish Kumar (201153) <mankumar@amdocs.com>
2023-10-12 16:32:10 +0530

    initial commit

---
+++ src/main/java/com/amdocs/buyyourphonebackend/BuyyourphoneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.amdocs.buyyourphonebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyyourphoneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyyourphoneBackendApplication.class, args);
+	}
+
+}
